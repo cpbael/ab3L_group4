b7d2cd40717feb91f24668b8d67b7e65001e9ca6
Tessa Mercado <thee.cado@gmail.com>
2013-03-03 18:58:34 +0800

    uhm

--- admin/admin.php
+++ admin/admin.php
@@ -6,7 +6,9 @@
 ?>
 
 	<span class = "allTextBlue"><a class = "button2" href="add_service.php">Add Service</a></span> <br/>
-	<span class = "allTextBlue"><a class = "button2" href="show_services.php">Show Services</a></span>
+	<span class = "allTextBlue"><a class = "button2" href="show_services.php">Show Services</a></span><br/>
+	<span class = "allTextBlue"><a class = "button2" href="view_services.php">Add Reservation</a></span><br/>
+	<span class = "allTextBlue"><a class = "button2" href="view_all_reservations.php">View Reservations</a></span><br/>
 	<span		>
 
 <?php require_once"footer.php";?>
